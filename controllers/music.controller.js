126b37e581b3d8726205734a48171401212f4863
sujal-17-nagpal <sujaln0609y@example.com>
2026-03-03 20:19:15 +0530

    music-upload api done

--- controllers/music.controller.js
+++ controllers/music.controller.js
@@ -1,5 +1,6 @@
 const musicModel = require("../model/music.model")
 const jwt = require("jsonwebtoken")
+const uploadFile = require("../services/storage.service")
 
 const createMusic = async(req,res)=>{
 
@@ -22,6 +23,24 @@ const createMusic = async(req,res)=>{
 
         const {title} = req.body
         const file = req.file
+
+        const result = await uploadFile(file.buffer.toString("base64"))
+
+        const music = await musicModel.create({
+            url : result.url,
+            title,
+            artist : decoded.id
+        })
+
+        res.status(201).json({
+            message:"music created successfully",
+            music : {
+                id : music._id,
+                title : music.title,
+                url : music.url,
+                artist : music.artist
+            }
+        })
 
     } catch (error) {
         console.log(error.message)
--- routes/music.routes.js
+++ routes/music.routes.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const { createMusic } = require("../controllers/music.controller")
+
+const multer = require('multer')
+
+const upload = multer({
+    storage : multer.memoryStorage()
+})
+
+
 
 const musicRouter = express.Router()
 
-
+musicRouter.post("/upload",upload.single("music"),createMusic)
 
 module.exports = musicRouter
